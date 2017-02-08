11a0b5601023e79f26b553c4c9bbe8c5f2f05614
Mike Borg <mikeborg85@gmail.com>
2017-02-08 17:46:18 -0500

    Maintaining the core hacks.

--- wp-admin/includes/class-wp-list-table.php
+++ wp-admin/includes/class-wp-list-table.php
@@ -343,28 +343,32 @@ class WP_List_Table {
 	 * @param string $text     The 'submit' button label.
 	 * @param string $input_id ID attribute value for the search input field.
 	 */
-	public function search_box( $text, $input_id ) {
-		if ( empty( $_REQUEST['s'] ) && !$this->has_items() )
-			return;
+    public function search_box( $text, $input_id ) {
+        if ( empty( $_REQUEST['s'] ) && !$this->has_items() )
+            return;
 
-		$input_id = $input_id . '-search-input';
+        $input_id = $input_id . '-search-input';
 
-		if ( ! empty( $_REQUEST['orderby'] ) )
-			echo '<input type="hidden" name="orderby" value="' . esc_attr( $_REQUEST['orderby'] ) . '" />';
-		if ( ! empty( $_REQUEST['order'] ) )
-			echo '<input type="hidden" name="order" value="' . esc_attr( $_REQUEST['order'] ) . '" />';
-		if ( ! empty( $_REQUEST['post_mime_type'] ) )
-			echo '<input type="hidden" name="post_mime_type" value="' . esc_attr( $_REQUEST['post_mime_type'] ) . '" />';
-		if ( ! empty( $_REQUEST['detached'] ) )
-			echo '<input type="hidden" name="detached" value="' . esc_attr( $_REQUEST['detached'] ) . '" />';
-?>
-<p class="search-box">
-	<label class="screen-reader-text" for="<?php echo esc_attr( $input_id ); ?>"><?php echo $text; ?>:</label>
-	<input type="search" id="<?php echo esc_attr( $input_id ); ?>" name="s" value="<?php _admin_search_query(); ?>" />
-	<?php submit_button( $text, '', '', false, array( 'id' => 'search-submit' ) ); ?>
-</p>
-<?php
-	}
+        if ( ! empty( $_REQUEST['orderby'] ) )
+            echo '<input type="hidden" name="orderby" value="' . esc_attr( $_REQUEST['orderby'] ) . '" />';
+        if ( ! empty( $_REQUEST['order'] ) )
+            echo '<input type="hidden" name="order" value="' . esc_attr( $_REQUEST['order'] ) . '" />';
+        if ( ! empty( $_REQUEST['post_mime_type'] ) )
+            echo '<input type="hidden" name="post_mime_type" value="' . esc_attr( $_REQUEST['post_mime_type'] ) . '" />';
+        if ( ! empty( $_REQUEST['detached'] ) )
+            echo '<input type="hidden" name="detached" value="' . esc_attr( $_REQUEST['detached'] ) . '" />';
+
+        if ($_REQUEST['post_type'] == 'shop_order') {
+            echo apply_filters('orderSearchHack', null);
+        } else { ?>
+            <p class="search-box">
+                <label class="screen-reader-text" for="<?php echo $input_id ?>"><?php echo $text; ?>:</label>
+                <input type="search" id="<?php echo $input_id ?>" name="s" value="<?php _admin_search_query(); ?>" />
+                <?php submit_button( $text, 'button', '', false, array('id' => 'search-submit') ); ?>
+            </p>
+        <?php
+        }
+    }
 
 	/**
 	 * Get an associative array ( id => link ) with the list
